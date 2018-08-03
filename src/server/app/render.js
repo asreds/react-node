68e7fd4f4cedb0fa96ccc5e28a999a0d2968f4b3
Asreds <asih.priyatno@sebangsa.com>
2018-08-03 15:52:53 +0700

    add script start app

--- src/server/app/render.js
+++ src/server/app/render.js
@@ -1,23 +1,26 @@
 #!/usr/bin/env node
 
-import React from "react";
-import ReactDOMServer from "react-dom/server";
-import { renderRoutes } from "react-router-config";
-import routes from "../../client/routes";
+// import React from "react";
+// import ReactDOMServer from "react-dom/server";
+// import { renderRoutes } from "react-router-config";
+// import routes from "../../client/routes";
 
 export default function handleRender(req, res, next) {
-  let html;
+  res.send(renderHtml('Hello word'))
+  
+  //let html;
 
-  return Promise.all(promises).then(() => {
-    // return respond html as string from react-dom-server
-    html = ReactDOMServer.renderToString(renderRoutes(routes));
-    if (context.url) {
-      res.status(500).end("internal server error");
-    } else {
-      //render html + preloaded state
-      res.send(renderHtml(html, store.getState(), styleTags));
-    }
-  });
+
+  // return Promise.all(promises).then(() => {
+  //   // return respond html as string from react-dom-server
+  //   html = ReactDOMServer.renderToString(renderRoutes(routes));
+  //   if (context.url) {
+  //     res.status(500).end("internal server error");
+  //   } else {
+  //     //render html + preloaded state
+  //     res.send(renderHtml(html, store.getState(), styleTags));
+  //   }
+  // });
 }
 
 // wrapper of render HTML
@@ -32,8 +35,6 @@ function renderHtml(body = "") {
         </head>
       <body>
         <div id="app">${body}</div>
-        <script src="${webpackAssets.vendor.js}"></script>
-        <script src="${webpackAssets.app.js}"></script>
       </body>
     </html>
   `.replace(/\s\s+/g, "");
